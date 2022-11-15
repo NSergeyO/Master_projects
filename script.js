d245b4225bf5c991351a505eb7fabf2c69092815
NSergeyO <s.o.nefedov@gmail.com>
2022-11-15 16:21:45 +0300

    added commit gitignore

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 console.log('Привет, СЕРГЕЙ!!! Теперь начнем работу с Git и GitHub');
-document.body.innerHTML = '<h1>Сергей, ты снова занимаешься Git and GitHub</h1>';
+// document.body.innerHTML = '<h1>Сергей, ты снова занимаешься Git and GitHub</h1>';
